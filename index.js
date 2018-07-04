1c7576258df1f4b6e14bf69991cf4f5c29739420
longbo@cloudcore.cn <lb2013@szcc>
2018-07-04 14:57:59 +0800

    relative path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
 
     /* ------- Preloader ------ */
     $(window).load(function(){
-        $('.preloader').delay(1000).slideUp('slow'); // set duration in brackets
+        $('.preloader').delay(250).slideUp('slow'); // set duration in brackets
     });
 
     /* -------- Appears Menu 滚动显示scroll ------ */
