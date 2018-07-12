6ba9305b83fad1425437a745adf7e96d9bfceb90
qqjunhui <626607098@qq.com>
2018-07-12 19:19:06 +0800

    首页部分修改

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     "use strict";
 
     /* ------- Preloader ------ */
-    $(window).load(function(){
+    $(window).load(function () {
         $('.preloader').delay(250).slideUp('slow'); // set duration in brackets
     });
 
+    window.addEventListener("scroll", function (e) {
+        var top = document.documentElement.scrollTop || document.body.scrollTop;
+        if (top > $(window).height()) {
+            $('.goTop').css('display','block');
+        }else{
+            $('.goTop').css('display','none');
+        }
+    });
     /* -------- Appears Menu 滚动显示scroll ------ */
-    $(window).on('ready , scroll', function() {
+    $(window).on('ready , scroll', function () {
         if ($(window).scrollTop() > 100) {
             $('.main-menu').addClass('minified');
         } else {
@@ -30,12 +38,12 @@ jQuery(document).ready(function() {
     /* ---------- Wow Js ---------- */
     var wow = new WOW(
         {
-            boxClass:     'wow',      // animated element css class (default is wow)
+            boxClass: 'wow',      // animated element css class (default is wow)
             animateClass: 'wow animated', // animation css class (default is animated)
-            offset:       250,          // distance to the element when triggering the animation (default is 0)
-            mobile:       true,       // trigger animations on mobile devices (default is true)
-            live:         true,       // act on asynchronously loaded content (default is true)
-            callback:     function(box) {
+            offset: 250,          // distance to the element when triggering the animation (default is 0)
+            mobile: true,       // trigger animations on mobile devices (default is true)
+            live: true,       // act on asynchronously loaded content (default is true)
+            callback: function (box) {
                 // the callback is fired every time an animation is started
                 // the argument that is passed in is the DOM node being animated
             }
@@ -58,7 +66,7 @@ jQuery(document).ready(function() {
     });
 
 
-    $('#portfolio-filter ul li a').on('click',function(){
+    $('#portfolio-filter ul li a').on('click', function () {
         var selector = $(this).attr('data-filter');
         $container.isotope({
             filter: selector,
@@ -74,10 +82,10 @@ jQuery(document).ready(function() {
     var $optionSets = $('#portfolio-filter ul'),
         $optionLinks = $optionSets.find('a');
 
-    $optionLinks.on('click',function(){
+    $optionLinks.on('click', function () {
         var $this = $(this);
         // don't proceed if already selected
-        if ( $this.hasClass('selected') ) {
+        if ($this.hasClass('selected')) {
             return false;
         }
         var $optionSet = $this.parents('#portfolio-filter ul');
@@ -98,7 +106,15 @@ jQuery(document).ready(function() {
         $('#svg-continer-2').removeClass('done');
         svgVobj2.reset();
         svgVobj2.play(1);
-    }
+    };
+    /*返回頂部*/
 
+    /*底部切换*/
+    $('.contact div').mouseover(function () {
+        $('.contact div').removeClass('active');
+        $(this).addClass('active');
+        $('footer .content p').removeClass('active');
+        $("footer .content p:eq(" + $(this).index() + ")").addClass('active');
+    })
 
 });
