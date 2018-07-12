69447914c2cc0645762c4781f808a3dc7cb8651d
longbo@cloudcore.cn <lb2013@szcc>
2018-07-12 11:30:40 +0800

    精简demo模版页

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,5 @@
 jQuery(document).ready(function() {
     "use strict";
-    /* ------- Preloader ------ */
-    $(window).load(function(){
-        $('.preloader').delay(1000).slideUp('slow'); // set duration in brackets
-    });
 
     /* -------- Appears Menu 滚动显示scroll ------ */
     $(window).on('ready , scroll', function() {
@@ -17,43 +13,6 @@ jQuery(document).ready(function() {
     /* ---------- Hide Menu-------- */
     jQuery(".nav a").on("click", function () {
         jQuery("#nav-menu").removeClass("in").addClass("collapse");
-    });
-
-    /* --------- One Page Navigation 页内锚点导航 -------- */
-    // $('#collapsingNavbar').onePageNav({
-    //     currentClass: 'active',
-    //     scrollSpeed: 500,
-    //     easing: 'linear'
-    // });
-
-    /* ---------- Wow Js ---------- */
-    var wow = new WOW(
-        {
-            boxClass:     'wow',      // animated element css class (default is wow)
-            animateClass: 'wow animated', // animation css class (default is animated)
-            offset:       250,          // distance to the element when triggering the animation (default is 0)
-            mobile:       true,       // trigger animations on mobile devices (default is true)
-            live:         true,       // act on asynchronously loaded content (default is true)
-            callback:     function(box) {
-                // the callback is fired every time an animation is started
-                // the argument that is passed in is the DOM node being animated
-            }
-        }
-    );
-    wow.init();
-
-    /* ---------- ISoptope --------- */
-    var $container = $('.portfolio-items');
-
-    // filter items on button click
-    $container.isotope({
-        filter: '*',
-        itemSelector: '.item',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
     });
 
     /*--------------底部切换-------------*/
